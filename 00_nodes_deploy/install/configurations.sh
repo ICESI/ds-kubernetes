e01e1ff41c1360ebd856f215d62de200b0c56e1a
daniel.barragan <daniel.barragan@alertlogic.com>
2018-04-27 18:30:27 -0500

    Update Files

--- 00_nodes_deploy/install/configurations.sh
+++ 00_nodes_deploy/install/configurations.sh
@@ -1,4 +1,4 @@
-echo "node0 192.168.56.101" >> /ettc/hosts
-echo "node1 192.168.56.102" >> /ettc/hosts
-echo "node2 192.168.56.103" >> /ettc/hosts
-echo "node3 192.168.56.104" >> /ettc/hosts
+echo "node0 192.168.56.101" >> /etc/hosts
+echo "node1 192.168.56.102" >> /etc/hosts
+echo "node2 192.168.56.103" >> /etc/hosts
+echo "node3 192.168.56.104" >> /etc/hosts
